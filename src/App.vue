5798cecc07e01ccbcc3e1ae1fff16139a3b641b1
rywaroy <464485682@qq.com>
2021-04-21 14:07:52 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>ui</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+
+</style>
