666eff10b3dda2de6c0e52c8220378e16c8db514
rywaroy <464485682@qq.com>
2021-04-22 14:32:05 +0800

    feat: element

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>ui</div>
+  <div class="content">
+    <div class="menu">
+      <el-menu default-active="数据展示">
+        <el-menu-item index="数据展示">
+          数据展示
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,4 +19,19 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+@import './assets/reset.css';
+.content {
+  width: 100vw;
+  height: 100vh;
+  background: #f5f5f5;
+}
+.menu {
+  width: 200px;
+  height: 100%;
+}
+
+.menu .el-menu {
+  height: 100%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import router from './router';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(ElementPlus);
+app.mount('#app');
