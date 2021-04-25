2c54b2de27221565050b92bdbb2b31c005352988
rywaroy <464485682@qq.com>
2021-04-25 15:49:34 +0800

    feat: 组件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import vueHljs from 'vue-hljs';
+import hljs from 'highlight.js';
+
 import App from './App.vue';
 import router from './router';
+import ComponentContainer from './component/ComponentContainer/index.vue';
+import InfoOverview from './packages/InfoOverview';
+import InfoOverviewItem from './packages/InfoOverviewItem';
 import 'element-ui/lib/theme-chalk/index.css';
+import 'vue-hljs/dist/style.css';
+import './assets/reset.css';
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(vueHljs, { hljs });
+Vue.component('ComponentContainer', ComponentContainer);
+
+Vue.use(InfoOverview);
+Vue.use(InfoOverviewItem);
 
 new Vue({
   router,
