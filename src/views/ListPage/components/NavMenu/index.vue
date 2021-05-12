1b28f7f4cdb45e32ed35aefd7c3c9a89441f7cca
张志豪 <zhangzhihao08@corp.netease.com>
2021-05-12 17:30:29 +0800

    feat: nav-menu 组件

---
+++ src/views/ListPage/components/NavMenu/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="nav-menu">
+    <el-menu
+      background-color="#2d4464"
+      text-color="#fff">
+      <el-menu-item index="/page1">
+        <span slot="title">页面1</span>
+      </el-menu-item>
+      <el-menu-item index="/page2">
+        <span slot="title">页面2</span>
+      </el-menu-item>
+      <el-menu-item index="/page3">
+        <span slot="title">页面3</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<style>
+.nav-menu {
+  height: 100%;
+}
+.nav-menu .el-menu {
+  height: 100%;
+}
+</style>
