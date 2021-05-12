b65669e69f5d4232ee6079589cd30575e0b8f158
张志豪 <zhangzhihao08@corp.netease.com>
2021-05-12 17:21:37 +0800

    feat: base-header 组件

---
+++ src/views/ListPage/components/BaseHeader/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="app-header">运营后台</header>
+</template>
+<style scoped>
+.app-header {
+  height: 64px;
+  background: #1d2b40;
+  font-size: 24px;
+  line-height: 64px;
+  color: #fff;
+  padding-left: 20px;
+}
+</style>
