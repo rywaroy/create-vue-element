44cc75073b362f43d1540b642bdedd804c852645
rywaroy <464485682@qq.com>
2021-05-13 10:08:31 +0800

    feat: 添加layout 组件

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import InfoOverviewItem from './packages/InfoOverviewItem';
 import InfoDetail from './packages/InfoDetail';
 import InfoDetailRow from './packages/InfoDetailRow';
 import InfoDetailItem from './packages/InfoDetailItem';
+import PageLayout from './packages/PageLayout';
 
 import 'element-ui/lib/theme-chalk/index.css';
 import 'vue-hljs/dist/style.css';
@@ -30,6 +31,7 @@ Vue.use(InfoOverviewItem);
 Vue.use(InfoDetail);
 Vue.use(InfoDetailRow);
 Vue.use(InfoDetailItem);
+Vue.use(PageLayout);
 
 new Vue({
   router,
