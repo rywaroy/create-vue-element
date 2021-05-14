1610e19e68937e6a00972a614cfa450eefd787d6
rywaroy <464485682@qq.com>
2021-05-14 17:39:56 +0800

    feat: 基本页面模型添加search form组件

--- src/views/ListPage/index.vue
+++ src/views/ListPage/index.vue
@@ -13,7 +13,9 @@
         <div class="list-page-content">
           <base-container name="page-layout">
             <ve-page-layout title="标题">
-
+              <base-container name="search-form">
+                <ve-search-form :form.sync="form" :fieldList.sync="fieldList"></ve-search-form>
+              </base-container>
             </ve-page-layout>
           </base-container>
         </div>
@@ -26,6 +28,68 @@ import BaseHeader from './components/BaseHeader';
 import NavMenu from './components/NavMenu';
 
 export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        sex: '',
+        age: [10, 18],
+        time: [],
+        姓名1: '',
+        姓名2: '',
+        年龄1: [1, 2],
+        年龄2: [3, 4],
+        时间1: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+        时间2: [],
+      },
+      fieldList: [
+        {
+          label: '姓名',
+          value: 'name',
+          type: 'input',
+        },
+        {
+          label: '性别',
+          value: 'sex',
+          type: 'select',
+          options: [{ label: '男', value: '男' }, { label: '女', value: '女' }],
+        },
+        {
+          label: '时间',
+          value: 'time',
+          type: 'range-time',
+          span: 12,
+        },
+        {
+          label: '年龄',
+          value: 'age',
+          type: 'range-number',
+        },
+        {
+          type: 'custom-label',
+          labelOptions: [{ label: '姓名1', value: '姓名1' }, { label: '姓名2', value: '姓名2' }],
+          id: '姓名',
+          value: '姓名1',
+          valueType: 'input',
+        },
+        {
+          type: 'custom-label',
+          labelOptions: [{ label: '年龄1', value: '年龄1' }, { label: '年龄2', value: '年龄2' }],
+          id: '年龄',
+          value: '年龄1',
+          valueType: 'range-number',
+        },
+        {
+          type: 'custom-label',
+          labelOptions: [{ label: '时间1', value: '时间1' }, { label: '时间2', value: '时间2' }],
+          id: '时间',
+          value: '时间1',
+          valueType: 'range-time',
+          span: 12,
+        },
+      ],
+    };
+  },
   components: {
     BaseHeader,
     NavMenu,
