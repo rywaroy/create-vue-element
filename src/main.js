f5581625f5f230e354b715ca1b4efaaa33332c4c
rywaroy <464485682@qq.com>
2021-05-17 10:38:31 +0800

    feat: use

--- src/main.js
+++ src/main.js
@@ -13,8 +13,7 @@ import InfoOverviewItem from './packages/InfoOverviewItem';
 import InfoDetail from './packages/InfoDetail';
 import InfoDetailRow from './packages/InfoDetailRow';
 import InfoDetailItem from './packages/InfoDetailItem';
-import PageLayout from './packages/PageLayout';
-import SearchForm from './packages/SearchForm';
+import Veui from './packages';
 
 import 'element-ui/lib/theme-chalk/index.css';
 import 'vue-hljs/dist/style.css';
@@ -32,8 +31,7 @@ Vue.use(InfoOverviewItem);
 Vue.use(InfoDetail);
 Vue.use(InfoDetailRow);
 Vue.use(InfoDetailItem);
-Vue.use(PageLayout);
-Vue.use(SearchForm);
+Vue.use(Veui);
 
 new Vue({
   router,
